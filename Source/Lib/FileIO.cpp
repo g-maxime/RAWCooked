d71e54f85e1b9069cfc2717ee5132b1778d0fccc
Jérôme Martinez <jerome@mediaarea.net>
2020-08-22 14:55:59 +0200

    TEMP

--- Source/Lib/FileIO.cpp
+++ Source/Lib/FileIO.cpp
@@ -32,48 +32,48 @@ int filemap::Open_ReadMode(const char* FileName)
     Close();
 
     size_t NewSize;
-    #if defined(_WIN32) || defined(_WINDOWS)
-        auto NewFile = CreateFileA(FileName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-        if (NewFile != INVALID_HANDLE_VALUE)
+#if defined(_WIN32) || defined(_WINDOWS)
+    auto NewFile = CreateFileA(FileName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    if (NewFile != INVALID_HANDLE_VALUE)
+    {
+        DWORD FileSizeHigh;
+        auto FileSizeLow = GetFileSize(NewFile, &FileSizeHigh);
+        if ((FileSizeLow != INVALID_FILE_SIZE || GetLastError() == NO_ERROR) // If no error (special case with 32-bit max value)
+            && (!FileSizeHigh || sizeof(size_t) >= 8)) // Mapping 4+ GiB files is not supported in 32-bit mode
         {
-            DWORD FileSizeHigh;
-            auto FileSizeLow = GetFileSize(NewFile, &FileSizeHigh);
-            if ((FileSizeLow != INVALID_FILE_SIZE || GetLastError() == NO_ERROR) // If no error (special case with 32-bit max value)
-             && (!FileSizeHigh || sizeof(size_t) >= 8)) // Mapping 4+ GiB files is not supported in 32-bit mode
+            NewSize = ((size_t)FileSizeHigh) << 32 | FileSizeLow;
+            if (NewSize)
             {
-                NewSize = ((size_t)FileSizeHigh) << 32 | FileSizeLow;
-                if (NewSize)
-                {
-                    auto Mapping = (HANDLE&)Private2;
-                    Mapping = CreateFileMapping(NewFile, 0, PAGE_READONLY, 0, 0, 0);
-                    if (!Mapping)
-                        Private = NewFile;
-                    else
-                        CloseHandle(NewFile);
-                }
+                auto Mapping = (HANDLE&)Private2;
+                Mapping = CreateFileMapping(NewFile, 0, PAGE_READONLY, 0, 0, 0);
+                if (!Mapping)
+                    Private = NewFile;
                 else
-                    Private = NewFile; // CreateFileMapping does not support 0-byte files, so we map manually to NULL
+                    CloseHandle(NewFile);
             }
             else
-            {
-                CloseHandle(NewFile);
-            }
+                Private = NewFile; // CreateFileMapping does not support 0-byte files, so we map manually to NULL
         }
-    #else
-        auto fd = open(FileName, O_RDONLY, 0);
-        if (fd != -1)
+        else
         {
-            struct stat Fstat;
-            if (!stat(FileName, &Fstat))
-            {
-                NewSize = Fstat.st_size;
-                Private = fd;
-            }
-            else
-                close(fd);
+            CloseHandle(NewFile);
         }
+    }
+#else
+    auto fd = open(FileName, O_RDONLY, 0);
+    if (fd != -1)
+    {
+        struct stat Fstat;
+        if (!stat(FileName, &Fstat))
+        {
+            NewSize = Fstat.st_size;
+            Private = fd;
+        }
+        else
+            close(fd);
+    }
 
-    #endif
+#endif
 
     if (!Private)
         return 1; 
@@ -86,11 +86,11 @@ int filemap::Remap(size_t NewSize)
     // Close previous map
     if (Data())
     {
-        #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
             UnmapViewOfFile(Data());
-        #else
+#else
             munmap(Data(), NewSize());
-        #endif
+#endif
         ClearBase();
     }
 
@@ -99,15 +99,15 @@ int filemap::Remap(size_t NewSize)
         return 0;
 
     // New map
-    #if defined(_WIN32) || defined(_WINDOWS)
-        auto Mapping = (HANDLE&)Private2;
-        auto NewData = MapViewOfFile(Mapping, FILE_MAP_READ, 0, 0, 0);
-        const decltype(NewData) NewData_Fail = 0;
-    #else
-        auto fd = (int&)Private;
-        auto NewData = mmap(NULL, NewSize, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
-        const decltype(NewData) NewData_Fail = MAP_FAILED;
-    #endif
+#if defined(_WIN32) || defined(_WINDOWS)
+    auto Mapping = (HANDLE&)Private2;
+    auto NewData = MapViewOfFile(Mapping, FILE_MAP_READ, 0, 0, 0);
+    const decltype(NewData) NewData_Fail = 0;
+#else
+    auto fd = (int&)Private;
+    auto NewData = mmap(NULL, NewSize, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
+    const decltype(NewData) NewData_Fail = MAP_FAILED;
+#endif
 
     // Assign
     if (NewData == NewData_Fail)
@@ -134,33 +134,33 @@ int filemap::Close()
     }
 
     // Close intermediate
-    #if defined(_WIN32) || defined(_WINDOWS)
-        auto Mapping = (HANDLE&)Private2;
-        if (Mapping)
-        {
-            CloseHandle(Mapping);
-            Private2 = NULL;
-        }
-    #endif
+#if defined(_WIN32) || defined(_WINDOWS)
+    auto Mapping = (HANDLE&)Private2;
+    if (Mapping)
+    {
+        CloseHandle(Mapping);
+        Private2 = NULL;
+    }
+#endif
 
     // Close file
     if (Private)
     {
-        #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
             auto File = (HANDLE&)Private;
             if (!CloseHandle(File))
             {
                 Private = nullptr;
                 return 1;
             }
-        #else
+#else
             int& fd = (int&)Private;
             if (close(fd))
             {
                 Private = (void*)-1;
                 return 1;
             }
-        #endif
+#endif
         Private = nullptr;
     }
 
@@ -176,18 +176,18 @@ file::return_value file::Open_WriteMode(const string& BaseDirectory, const strin
 
     string FullName = BaseDirectory + OutputFileName_Source;
 
-    #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
     HANDLE& P = (HANDLE&)Private;
     DWORD CreationDisposition = Truncate ? (RejectIfExists ? TRUNCATE_EXISTING : CREATE_ALWAYS) : (RejectIfExists ? CREATE_NEW : OPEN_ALWAYS);
     P = CreateFileA(FullName.c_str(), GENERIC_WRITE, 0, 0, CreationDisposition, FILE_ATTRIBUTE_NORMAL, 0);
     if (P == INVALID_HANDLE_VALUE)
-    #else
+#else
     int& P = (int&)Private;
     const int flags = O_WRONLY | O_CREAT | (RejectIfExists ? O_EXCL : 0) | (Truncate ? O_TRUNC : 0);
     const mode_t Mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
     P = open(FullName.c_str(), flags, Mode);
     if (P == -1)
-    #endif
+#endif
     {
         size_t i = 0;
         for (;;)
@@ -209,13 +209,13 @@ file::return_value file::Open_WriteMode(const string& BaseDirectory, const strin
                 }
             }
         }
-        #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
         P = CreateFileA(FullName.c_str(), GENERIC_WRITE, 0, 0, CreationDisposition, FILE_ATTRIBUTE_NORMAL, 0);
         if (P == INVALID_HANDLE_VALUE)
-        #else
+#else
         P = open(FullName.c_str(), flags, Mode);
         if (P == -1)
-        #endif
+#endif
         {
             Private = (void*)-1;
             return (access(FullName.c_str(), 0)) ? (RejectIfExists ? Error_FileCreate : Error_FileWrite) : Error_FileAlreadyExists;
@@ -237,7 +237,7 @@ file::return_value file::Write(const uint8_t* Data, size_t Size)
 
     // Check that a file is open
     size_t Offset = 0;
-    #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
     HANDLE& P = (HANDLE&)Private;
     BOOL Result;
     while (Offset < Size)
@@ -254,7 +254,7 @@ file::return_value file::Write(const uint8_t* Data, size_t Size)
         Offset += BytesWritten;
     }
     if (Result == FALSE || Offset < Size)
-    #else
+#else
     int& P = (int&)Private;
     ssize_t BytesWritten;
     while (Offset < Size)
@@ -266,7 +266,7 @@ file::return_value file::Write(const uint8_t* Data, size_t Size)
         Offset += (size_t)BytesWritten;
     }
     if (BytesWritten == -1 || Offset < Size)
-    #endif
+#endif
     {
         return Error_FileWrite;
     }
@@ -282,12 +282,12 @@ file::return_value file::Seek(int64_t Offset, seek_value Method)
     if (Private == (void*)-1)
         return Error_FileCreate;
         
-    #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
     HANDLE& P = (HANDLE&)Private;
     LARGE_INTEGER Offset2;
     Offset2.QuadPart = Offset;
     if (SetFilePointerEx(P, Offset2, NULL, Method) == 0)
-    #else
+#else
     int& P = (int&)Private;
     int whence;
     switch (Method)
@@ -298,7 +298,7 @@ file::return_value file::Seek(int64_t Offset, seek_value Method)
         default      : whence =ios_base::beg;
     }
     if (lseek(P, Offset, whence) == (off_t)-1)
-    #endif
+#endif
     {
         return Error_Seek;
     }
@@ -314,10 +314,10 @@ file::return_value file::SetEndOfFile()
     if (Private == (void*)-1)
         return Error_FileCreate;
 
-    #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
     HANDLE& P = (HANDLE&)Private;
     if (!::SetEndOfFile(P))
-    #else
+#else
     int& P = (int&)Private;
     off_t length = lseek(P, 0, SEEK_CUR);
     if (length == (off_t)-1)
@@ -325,7 +325,7 @@ file::return_value file::SetEndOfFile()
         return Error_FileWrite;
     }
     if (ftruncate(P, length))
-    #endif
+#endif
     {
         return Error_FileWrite;
     }
@@ -341,13 +341,13 @@ file::return_value file::Close()
     if (Private == (void*)-1)
         return OK;
         
-    #if defined(_WIN32) || defined(_WINDOWS)
+#if defined(_WIN32) || defined(_WINDOWS)
     HANDLE& P = (HANDLE&)Private;
     if (CloseHandle(P) == 0)
-    #else
+#else
     int& P = (int&)Private;
     if (close(P))
-    #endif
+#endif
     {
         Private = (void*)-1;
         OutputFileName.clear();
